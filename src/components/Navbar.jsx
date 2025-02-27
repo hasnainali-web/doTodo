0a11ec47c83b5137717c2c5046c48f33596ed92a
hasnainali341 <ha489461@gmail.com>
2025-02-27 23:47:28 +0500

    Minor Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,8 @@ import React, { useEffect, useRef } from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-[#FED36A] text-black flex items-center justify-between p-3 sm:px-4 md:px-6 lg:px-8 rounded-b-md lg:rounded-b-lg text-sm lg:text-base ">
+      <nav className="bg-[#FED36A] text-black flex items-center justify-center p-3 sm:px-4 md:px-6 lg:px-8 rounded-b-md lg:rounded-b-lg text-sm lg:text-base ">
         <div className="logo cursor-pointer font-bold tracking-wide">doTodo</div>
-        <ul className="flex space-x-4">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Tasks</li>
-        </ul>
       </nav>
     </>
   );
