40e2fd394135d57a6a0ec1acee34629e3f1e61e8
hasnainali-web <hasnainali.itdeveloper@gmail.com>
2025-02-28 00:01:24 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-[#FED36A] text-black flex items-center justify-center p-3 sm:px-4 md:px-6 lg:px-8 rounded-b-md lg:rounded-b-lg text-sm lg:text-base ">
+      <nav className="bg-[#FED36A] text-black flex items-center justify-center p-3 sm:px-4 md:px-6 lg:px-8 rounded-b-md lg:rounded-b-lg text-sm lg:text-base selection:bg-black selection:text-[#FED36A]">
         <div className="logo cursor-pointer font-bold tracking-wide">doTodo</div>
       </nav>
     </>
